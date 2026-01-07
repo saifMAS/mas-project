47bb1a5b346d1ceb77217fd49a79cb7cd6290853
saif <saifratb2005@gmail.com>
2026-01-07 17:29:18 +0300

    Enable CORS for admin panel

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000
 // middleware: JSON + ملفات static
 app.use(express.json());
